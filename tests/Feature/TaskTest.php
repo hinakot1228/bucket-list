95bf1afc064d9b3bc939e5a8e9a923302d0dd5b7
Hinako-Tanaka <youngbird1228@gmail.com>
2021-03-13 23:27:03 +0900

    second commit

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -15,7 +15,7 @@ class TaskTest extends TestCase
     /**
      * 各テストメソッドの実行前に呼ばれる
      */
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
 
@@ -38,3 +38,20 @@ class TaskTest extends TestCase
             'due_date' => '期限日 には日付を入力してください。',
         ]);
     }
+
+    /**
+     * 期限日が過去日付の場合はバリデーションエラー
+     * @test
+     */
+    public function due_date_should_not_be_past()
+    {
+        $response = $this->post('/folders/1/tasks/create', [
+            'title' => 'Sample task',
+            'due_date' => Carbon::yesterday()->format('Y/m/d'), // 不正なデータ（昨日の日付）
+        ]);
+
+        $response->assertSessionHasErrors([
+            'due_date' => '期限日 には今日以降の日付を入力してください。',
+        ]);
+    }
+}
